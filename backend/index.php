1f1de7e8c64d550da8abfdafc4f06137ec2b2f46
Loki Jiang <37170364+summer10920@users.noreply.github.com>
2020-12-16 10:12:26 +0800

    Initial commit

---
+++ backend/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="#" type="image/x-icon">
+    <title>履歷表後台管理</title>
+</head>
+<body>
+<h1>履歷表管理後台</h1>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="#" type="image/x-icon">
+    <title>我的線上履歷表</title>
+</head>
+<body>
+    <h1>線上履歷表</h1>
+</body>
+</html>
