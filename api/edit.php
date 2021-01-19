460f32a8f29ecf5eccd9ab02f9659a4fa841dfdc
林伶 <linling@linlingdeMacBook-Pro.local>
2021-01-19 20:41:26 +0800

    完成彈出視窗

---
+++ api/edit.php
@@ -0,0 +1,6 @@
+<?php
+include_once "../base.php";
+
+print_r($_POST);
+
+?>
---
+++ popup/banner.php
@@ -0,0 +1,18 @@
+<h3>新增banner</h3>
+<hr>
+<form action="./api/add.php" method="post" enctype="multipart/form-data">
+  <table>
+    <tr>
+      <td>loremloremloremloremlorem</td>
+      <td>loremloremloremloremlorem</td>
+    </tr>
+    <tr>
+      <td>loremloremloremloremlorem</td>
+      <td>loremloremloremloremlorem</td>
+    </tr>
+    <tr>
+      <td>loremloremloremloremlorem</td>
+      <td>loremloremloremloremlorem</td>
+    </tr>
+  </table>
+</form>
